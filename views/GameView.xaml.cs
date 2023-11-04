3a50c73b22453b8fac07f7caa29388eb0599515b
Sebastian Potrykus <seba05092001@gmail.com>
2023-11-04 20:58:32 +0100

    Size scaling added

--- views/GameView.xaml.cs
+++ views/GameView.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,16 +25,18 @@ namespace PlaNet_Projekt_1.views
     {
         private ObservableCollection<CellItem> _cells = new ObservableCollection<CellItem>();
         private Board Board { get; set; }
+        private double BoardWidth;
+        private double BoardHeight;
+        private bool Auto;
 
         public GameView(Board board)
         {
             InitializeComponent();
 
             Board = board;
+            Auto = false;
 
             BoardItemsControl.ItemsSource = _cells;
-
-            UpdateBoardItemsControl();
         }
 
         private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
@@ -49,16 +52,23 @@ namespace PlaNet_Projekt_1.views
         private void UpdateBoardItemsControl()
         {
             _cells.Clear();
+
+            var widthScale = BoardWidth/Board.Width;
+            var heightScale = BoardHeight / Board.Height;
+            var size = widthScale < heightScale ? widthScale : heightScale;
+            var widthOffset = widthScale > heightScale ? (BoardWidth - (size *  Board.Width)) / 2 : 0;
+            var heightOffset = widthScale < heightScale ? (BoardHeight - (size * Board.Height)) / 2 : 0;
+
             for (int i = 0; i < Board.Height; i++)
             {
                 for (int j = 0; j < Board.Width; j++)
                 {
                     _cells.Add(new CellItem()
                     {
-                        Width = 20,
-                        Height = 20,
-                        Left = 20 * j + 1,
-                        Top = 20 * i + 1,
+                        Width = size - 1,
+                        Height = size - 1,
+                        Left = size * j + 2 + widthOffset,
+                        Top = size * i + 2 + heightOffset,
                         Color = Board.States[Board.States.Count - 1].Cells[j, i] == true ? new SolidColorBrush(Colors.Black) : new SolidColorBrush(Colors.White),
                         X = j,
                         Y = i
@@ -66,6 +76,26 @@ namespace PlaNet_Projekt_1.views
                 }
             }
             StatGrid.DataContext = Board.States[Board.States.Count - 1];
+        }
+
+        private void BoardItemsControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            ItemsControl itemsControl = (ItemsControl)sender;
+
+            BoardWidth = itemsControl.ActualWidth;
+            BoardHeight = itemsControl.ActualHeight;
+
+            UpdateBoardItemsControl();
+        }
+
+        private void BoardItemsControl_SizeChanged(object sender, RoutedEventArgs e)
+        {
+            ItemsControl itemsControl = (ItemsControl)sender;
+
+            BoardWidth = itemsControl.ActualWidth;
+            BoardHeight = itemsControl.ActualHeight;
+
+            UpdateBoardItemsControl();
         }
 
         private void NextGen_Click(object sender, RoutedEventArgs e)
@@ -78,6 +108,17 @@ namespace PlaNet_Projekt_1.views
         {
             Board.PrevGen();
             UpdateBoardItemsControl();
+        }
+
+        private void AutoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Auto = true;
+            while (true)
+            {
+                if(Auto == false) return;
+                Board.NextGen();
+                UpdateBoardItemsControl();
+            }
         }
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
